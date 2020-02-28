c8054acae8d463a7199db8adb30628034db85da5
Libb521 <eotieno39@yahoo.com>
2020-02-28 14:49:29 +0300

    created a function that converts dates to days

--- images/views.py
+++ images/views.py
@@ -8,6 +8,8 @@ def welcome(request):
 #..........
 def today_images(request):
     date = dt.date.today()
+
+    day = convert_dates(date)
     html = f'''
         <html>
             <body>
@@ -16,3 +18,15 @@ def today_images(request):
         </html>
             '''
     return HttpResponse(html)
+
+def convert_dates(dates):
+
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
+
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day
+
