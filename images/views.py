bf32bfd05433764499285081fecf1172d47615dc
Libb521 <eotieno39@yahoo.com>
2020-03-01 18:19:18 +0300

    created other view functions linking them to the html files

--- images/views.py
+++ images/views.py
@@ -48,3 +48,20 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
+def gallery(request):
+    list = Album.objects.filter(is_visible=True).order_by('-created')
+    paginator = Paginatior(list,8)
+
+    oafe = request.Gert.get('page')
+    try:
+        albums = paginator.page(page)
+    except PageNotAnInteger:
+        albums = paginator.page(1)
+    except EmptyPage:
+        albums = paginator.page(paginator.num_pages)
+    return render(request, 'gallery.html', {'albums': list })
+
+def handler404(request, exception):
+    assert isinstance(request, HttpRequest)
+    return render(request, 'handler404.html', None, None, 404)
+
