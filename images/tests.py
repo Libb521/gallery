e8b51bad0dda8eb9236432343872ddba8fed18b7
Libb521 <eotieno39@yahoo.com>
2020-03-01 17:52:06 +0300

    created a sample test for the gallery

--- images/tests.py
+++ images/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from .models import Gallery, GalleryImage, tags
 
 # Create your tests here.
+class GalleryTestClass(TestCase):
+    def setUp(self):
+        self.fun=Gallery(title = 'fun')
+
+        # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.fun,Gallery))
+
