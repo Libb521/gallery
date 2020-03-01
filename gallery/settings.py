975d9a4742f48753e9f455a82fc4bd42117ccca8
Libb521 <eotieno39@yahoo.com>
2020-03-01 15:01:42 +0300

    created my first class in the models.py file to give the categories that will appear on the admin page

--- gallery/settings.py
+++ gallery/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'images',
     'bootstrap3',
+    'imagekit',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- images/models.py
+++ images/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFit
 
 # Create your models here.
+class Gallery(models.Model):
+    title = models.CharField(max_length=60)
+    description = models.TextField(max_length=600)
+    thumb = ProcessedImageField(upload_to='albums', processors=[ResizeToFit(500)], format='JPEG', options={'quality': 90})
+    tags = models.CharField(max_length=300)
+    createad = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=60, unique=True)
+
